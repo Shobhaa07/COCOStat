4c91f3ff9d7d4a4f14ed23bd6183d3af16d6e8f3
Shobhaa07 <165816352+Shobhaa07@users.noreply.github.com>
2026-03-27 19:54:12 +0530

    Update cocostat_app.py

--- cocostat_app.py
+++ cocostat_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
-from plotly.subplots 
+from plotly.subplots import make subplots
 from datetime import datetime, timedelta
 import io
 from streamlit_autorefresh import st_autorefresh
